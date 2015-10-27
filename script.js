6b8043ebcd967a7e6b0cc35ddeff141143005c70
Alice Frumin <alifrumin@yahoo.com>
2015-10-26 21:49:45 -0400

    edited css added more colors

--- script.js
+++ script.js
@@ -9,42 +9,55 @@ $(document).ready(function(){
       },
     salmon = {
       colorBoxColor:"salmon",
-      hint:"Like the fish",
+      hint:"Like the fish"
+      },
+    tomato = {
+      colorBoxColor:"tomato",
+      hint:"often used to make pasta sauce"
+      },
+    thistle = {
+      colorBoxColor:"thistle",
+      hint:"___ and Thyme"
+      },
+    lawngreen = {
+      colorBoxColor:"lawngreen",
+      hint:"front ___ + color"
+      },
+    dodgerblue = {
+      colorBoxColor:"dodgerblue",
+      hint:"LA baseball team + color"
+      },
+    ghostwhite = {
+      colorBoxColor:"ghostwhite",
+      hint:"pale as a ghost"
+      },
+    gainsboro = {
+      colorBoxColor:"gainsboro",
+      hint:"gainsboro"
       }
   ];
+
   //start
   $("#colorBox").css("background", colorPrompts[i].colorBoxColor)
   $(".hintDisplay").html(colorPrompts[i].hint)
   console.log(colorPrompts[i]);
 
+  //iterate through array
   function next(i){
   $("#colorBox").css("background", colorPrompts[i].colorBoxColor)
   $(".hintDisplay").html(colorPrompts[i].hint)
   }
-  // // next item in Array
-  // function next(i){
-  //   $("#colorBox").css("background", this.colorBoxColor);
-  //   $(".hintDisplay").html("<p>"+this.hint+"</p>");
-  //   i++;
-  // }
 
   //Length of Question Array for Number of Questions
   $("#totalQuestions").append(colorPrompts.length);
 
-  //check answer
+  //check answer on submit
   $(".submit").on("click", function(){
     if
     (($(".answer").val()) === (colorPrompts[i].colorBoxColor))
     {console.log(colorPrompts[i].colorBoxColor);
     i++;
     next(i);
-    // next();
-    // console.log(i);
-    // next();
-      // for (var i = 0; i < colorPrompts.length; i++) {
-      //   colorPrompts[i]
-      // };
-    //move to next item in Array
     //+1 to #correctQuestions
     //+1 to #answeredQuestions
     }
@@ -60,4 +73,11 @@ $(document).ready(function(){
   $(".hintButton").on("click", function(){
     $(".hintDisplay").toggle()
   });
+
+  //Skip button
+  $(".skip").on("click", function(){
+    i++;
+    next(i);
+  });
+
 });
